cbb652cc1a6ff0371f65b6195bdb7bae62fadb30
GhostBlank11 <37052551+GhostBlank11@users.noreply.github.com>
2024-12-13 13:34:01 +0700

    Update worker-with-socks5-experimental.js

--- src/worker-with-socks5-experimental.js
+++ src/worker-with-socks5-experimental.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'fb6704a3-c811-4ff5-8101-55575358fad6';
 
 let proxyIP = '';
 
